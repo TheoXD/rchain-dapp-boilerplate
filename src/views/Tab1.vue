19b9ff1c0fb3a62e9df7c2d8fca74dea01e2e4e4
Theo Hallenius <theo.hallenius@gmail.com>
2020-11-06 15:29:34 +0000

    Added menu and added place order button on each

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -12,17 +12,24 @@
         </ion-toolbar>
       </ion-header>
     
-        <ion-button @click="getLatestBlock">{{state.latestBlockNumber}}</ion-button>
-        <ion-button @click="deployRholang">Deploy some rholang</ion-button>
+        <ion-card v-for="pizza in state.menu" :key="pizza">
+            <img src="/assets/papperoni.jpg">
+            <ion-card-header>
+                <h2>{{pizza.name}}</h2>
+            </ion-card-header>
 
-      <ExploreContainer name="Tab 1 page" />
+            <ion-card-content>
+                {{pizza.ingredients}}
+                <ion-button @click="placeOrder(pizza.name)">Place Order</ion-button>
+            </ion-card-content>
+        </ion-card>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 import * as rchainToolkit from 'rchain-toolkit';
 
@@ -30,26 +37,45 @@ import {reactive} from 'vue';
 
 export default {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  props: {
+
+  },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   setup() {
       const state = reactive({
-            latestBlockNumber: "Get latest block"
+            latestBlockNumber: "Get latest block",
+            menu: [{
+                name: "Papperoni",
+                ingredients: "..."
+            },
+            {
+                name: "Small Sausage",
+                ingredients: "..."
+            }]
       });
 
-      const getLatestBlock = async () => {
+      const placeOrder = async (name) => {
         const latestBlock = JSON.parse(await rchainToolkit.http.blocks(`https://node0.testnet.rchain-dev.tk`, {
             position: 1,
         }));
         state.latestBlockNumber = latestBlock[0].blockNumber;
-      }
 
-      const deployRholang = async () => {
           const deployOptions = rchainToolkit.utils.getDeployOptions(
             "secp256k1",
             new Date().valueOf(),
-            `new hello, stdout(\`rho:io:stdout\`) in {
-            hello!("world") |
-            stdout!(*hello)
+            `new return(\`rho:rchain:deployId\`),
+            lookup(\`rho:registry:lookup\`)
+            in {
+            new valueCh, result in {
+                lookup!("rho:id:t6owcw7yuz57cn6zwt7cwjeqdczeyyzhwprajdbz1rppa3obuuwwn8" , *valueCh) |
+                for (pizzaShop <- valueCh) {
+                pizzaShop!("${name.toLowerCase()}", *result)
+                | 
+                for (@order <- result) {
+                    return!(order)
+                }
+                }
+            }
             }`,
             "e9934d79b12317d0f3dad7f915c1cc075a47cbd884fd80d7dfeefacf03fa214e",
             "0420d065ce52c1759e467b9427f494ff665210d226967e68b65eee180a96a3148fe849d1f105a7f130db938b48eb6c28f20dd530365aaeb39af8bfc00117684b18",
@@ -69,7 +95,7 @@ export default {
         }
       }
 
-      return {state, getLatestBlock, deployRholang}
+      return {state, placeOrder}
   }
 }
 </script>
