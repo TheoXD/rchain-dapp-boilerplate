a1a1408f73e62067f35c3dfdcd2e2644eadace25
Theo Hallenius <theo.hallenius@gmail.com>
2020-11-06 14:56:11 +0000

    deployRholang()

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -13,6 +13,7 @@
       </ion-header>
     
         <ion-button @click="getLatestBlock">{{state.latestBlockNumber}}</ion-button>
+        <ion-button @click="deployRholang">Deploy some rholang</ion-button>
 
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
@@ -32,7 +33,7 @@ export default {
   components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   setup() {
       const state = reactive({
-            latestBlockNumber: String
+            latestBlockNumber: "Get latest block"
       });
 
       const getLatestBlock = async () => {
@@ -42,7 +43,33 @@ export default {
         state.latestBlockNumber = latestBlock[0].blockNumber;
       }
 
-      return {state, getLatestBlock}
+      const deployRholang = async () => {
+          const deployOptions = rchainToolkit.utils.getDeployOptions(
+            "secp256k1",
+            new Date().valueOf(),
+            `new hello, stdout(\`rho:io:stdout\`) in {
+            hello!("world") |
+            stdout!(*hello)
+            }`,
+            "e9934d79b12317d0f3dad7f915c1cc075a47cbd884fd80d7dfeefacf03fa214e",
+            "0420d065ce52c1759e467b9427f494ff665210d226967e68b65eee180a96a3148fe849d1f105a7f130db938b48eb6c28f20dd530365aaeb39af8bfc00117684b18",
+            1,
+            100000,
+            state.latestBlockNumber
+        );
+        try {
+            const response = await rchainToolkit.http.deploy(
+                "https://node0.testnet.rchain-dev.tk",
+                deployOptions
+            );
+            console.info(response);
+        }
+        catch(err) {
+            console.err(err);
+        }
+      }
+
+      return {state, getLatestBlock, deployRholang}
   }
 }
 </script>
